4169c835eab8b237ee75fa009246cef62f23aec4
diegorezm <dmourarezende@gmail.com>
2023-12-13 15:44:39 -0300

    keys function

--- code/sec7/hashtable.py
+++ code/sec7/hashtable.py
@@ -13,18 +13,33 @@ class HashTable():
     def set(self, key: str, value: int):
         idx= self.__hash(key)
         if not self.data[idx]:
-            self.data[idx] = [key,value]
+            self.data[idx] = [[key,value]]
         else:
             self.data[idx] += [[key,value]]
-        print(self.data)
 
     def get(self, key:str):
         idx = self.__hash(key)
-        return self.data[idx][1]
+        addr = self.data[idx]
+        if addr is not None:
+            for i in range(len(addr)):
+                if addr[i][0] == key:
+                    return addr[i][1]
+        return None
+    
+    def keys(self):
+        keys = []
+        for i in range(self.size):
+            if self.data[i] is not None:
+                keys.append(self.data[i][0][0])
+        return keys
 
-hash = HashTable(2)
+    def print(self):
+        return self.data
+
+hash = HashTable(30)
 hash.set("oi", 100)
-hash.set("oi2", 100)
-hash.set("flw", 200)
-print(hash.get("flw"))
-
+hash.set("o1i", 300)
+hash.set("oi2", 380)
+hash.set("flw", 2000)
+hash.set("flw2", 3000)
+print(hash.keys())
