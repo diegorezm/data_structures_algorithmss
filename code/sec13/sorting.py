a7de34989a85294cf5bdb03f370feed69ec6829c
diegorezm <dmourarezende@gmail.com>
2023-12-19 10:27:04 -0300

    merge sort

--- code/sec13/sorting.py
+++ code/sec13/sorting.py
@@ -15,6 +15,7 @@ def selection_sort(arr: list[int]) -> list[int]:
                 min_index = j
         arr[i], arr[min_index] = arr[min_index] ,arr[i]
     return arr
+
 def insertion_sort(arr: list[int]) -> list[int]:
     for i in range(1, len(arr) - 1):
         key = arr[i]
@@ -25,7 +26,35 @@ def insertion_sort(arr: list[int]) -> list[int]:
         arr[j+1] = key
     return arr
 
+def merge_sort(arr: list[int]):
+    if len(arr) == 1:
+        return arr
+    m = len(arr) // 2
+    left = arr[:m]
+    right = arr[m:]
+    left = merge_sort(left)
+    right = merge_sort(right)
+    return merge(left, right)
+
+def merge(left: list[int], right:list[int]):
+    sorted_arr = []
+    i ,j = 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] > right[j]:
+            sorted_arr.append(right[j])
+            j += 1
+        else:
+            sorted_arr.append(left[i])
+            i += 1
+    while i < len(left):
+        sorted_arr.append(left[i])
+        i += 1
+    while j < len(right):
+        sorted_arr.append(right[j])
+        j += 1
+    return sorted_arr
+
 # print(bubble_sort([2,1,5,4,6,8,7,10,9,3]))
 # print(selection_sort([2,1,5,4,6,8,7,10,9,3]))
-print(insertion_sort([99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]))
-
+# print(insertion_sort([99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]))
+print(merge_sort([2,1,5,4,6,8,7,10,9,3]))
