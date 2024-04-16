a132e9564843955a934863e1ad8579fc035c1013
diegorezm <dmourarezende@gmail.com>
2024-04-15 23:56:28 -0300

    queue

--- code/c/queue/main.cpp
+++ code/c/queue/main.cpp
@@ -1,34 +1,44 @@
-#include <cstddef>
 #include <iostream>
-class Queue {
+template <typename T> class Queue {
+private:
+  int size;
+  T *queue;
+  int first;
+  int last;
+
 public:
   Queue(int size) {
     this->size = size;
-    this->queue = new int[size];
-    this->first = 0;
-    this->last = 0;
+    this->queue = new T[size];
+    this->first = this->last = -1;
   }
 
   ~Queue() { delete[] this->queue; }
 
-  int push(int n) {
-    int temp = -1;
-    if (this->last == this->size) {
+  void push(T n) {
+    if (this->first == 0 && this->last == this->size - 1) {
+      return;
+    } else if (this->first == -1) {
+      this->first = this->last = 0;
+      this->queue[this->last] = n;
+    } else if (this->first != 0 && this->last == this->size - 1) {
       this->last = 0;
-      temp = this->queue[0];
+      this->queue[this->last] = n;
+    } else {
+      this->last++;
+      this->queue[this->last] = n;
     }
-    this->queue[this->last] = n;
-    this->last++;
-    return temp;
   }
 
-  int pop() {
+  T pop() {
     if (this->isEmpty()) {
       return -1;
     }
-    int temp = this->queue[this->first];
+    T temp = this->queue[this->first];
     this->queue[this->first] = 0;
-    if (this->first > size) {
+    if (this->first == this->last) {
+      this->first = this->last = -1;
+    } else if (this->first == this->size - 1) {
       this->first = 0;
     } else {
       this->first++;
@@ -36,14 +46,14 @@ public:
     return temp;
   }
 
-  int peek() {
+  T peek() {
     if (this->isEmpty()) {
       return -1;
     }
     return this->queue[this->first];
   }
 
-  int index(int n) {
+  int index(T n) {
     int idx = -1;
     for (int i = 0; i < this->size; i++) {
       if (this->queue[i] == n) {
@@ -54,9 +64,12 @@ public:
     return idx;
   }
 
-  bool isEmpty() { return this->last == 0 && this->first == 0; }
+  bool isEmpty() { return this->first < 0; }
 
   bool isFull() { return this->last == this->size; }
+
+  int getFirstIndex() { return this->first; }
+  int getLastIndex() { return this->last; }
 
   void display() {
     std::cout << "First: " << this->first << std::endl;
@@ -69,16 +82,10 @@ public:
     }
     std::cout << std::endl;
   }
-
-private:
-  int size;
-  int *queue;
-  int first;
-  int last;
 };
 
 void josephus(int n, int n_people) {
-  Queue q(n_people - 1);
+  Queue<int> q(n_people - 1);
   for (int i = 0; i < n_people; i += n) {
     if (i > 0) {
       q.push(i);
@@ -92,7 +99,13 @@ void josephus(int n, int n_people) {
   q.display();
 }
 
-int main(int argc, char *argv[]) { 
-  josephus(2, 13);
-  return 0; 
+int main(int argc, char *argv[]) {
+
+  Queue<int> q(3);
+  q.push(1);
+  q.push(2);
+  q.pop();
+  q.push(3);
+  q.display();
+  return 0;
 }
